94b1cf9bc2123dde81501aaf31d9ae3d1728801a
daniel <danielngun04@gmail.com>
2025-11-03 19:12:11 +0200

    adding te data initializer

--- volunteersync/src/main/java/com/daniel4k/volunteersync/volunteersync/util/JwtUtil.java
+++ volunteersync/src/main/java/com/daniel4k/volunteersync/volunteersync/util/JwtUtil.java
@@ -14,7 +14,7 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
     private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 10; // 10 hours
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 10;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
