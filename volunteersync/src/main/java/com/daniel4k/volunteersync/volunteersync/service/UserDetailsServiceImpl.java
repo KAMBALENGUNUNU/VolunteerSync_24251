3d422b7c45783ada8f78df972b22fc5f5a4293d4
daniel <danielngun04@gmail.com>
2025-11-05 15:47:32 +0200

    update

--- volunteersync/src/main/java/com/daniel4k/volunteersync/volunteersync/service/UserDetailsServiceImpl.java
+++ volunteersync/src/main/java/com/daniel4k/volunteersync/volunteersync/service/UserDetailsServiceImpl.java
@@ -27,7 +27,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             throw new UsernameNotFoundException("User not found with email: " + email);
         }
 
-        return User.builder().username(volunteer.getEmail()).password(volunteer.getPassword()).authorities(Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + volunteer.getRole().name())))
-                .build();
+        return User.builder().username(volunteer.getEmail()).password(volunteer.getPassword()).authorities(Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + volunteer.getRole().name()))).build();
     }
 }
