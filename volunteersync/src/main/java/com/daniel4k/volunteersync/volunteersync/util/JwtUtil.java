e4f0dcd5ee83e54f4149f3547491c664ff15b906
daniel <danielngun04@gmail.com>
2025-11-05 15:54:36 +0200

    update

--- volunteersync/src/main/java/com/daniel4k/volunteersync/volunteersync/util/JwtUtil.java
+++ volunteersync/src/main/java/com/daniel4k/volunteersync/volunteersync/util/JwtUtil.java
@@ -43,8 +43,7 @@ public class JwtUtil {
     }
 
     private String createToken(Map<String, Object> claims, String subject) {
-        return Jwts.builder().setClaims(claims).setSubject(subject)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(SECRET_KEY)
                 .compact();
