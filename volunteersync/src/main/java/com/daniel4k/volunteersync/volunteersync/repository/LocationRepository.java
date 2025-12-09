385d8444b2a90efa8f33d6e42640d72b3697c38c
daniel <danielngun04@gmail.com>
2025-12-09 20:50:39 +0200

    refactor: add methods to fetch locations by type and parent ID

--- volunteersync/src/main/java/com/daniel4k/volunteersync/volunteersync/repository/LocationRepository.java
+++ volunteersync/src/main/java/com/daniel4k/volunteersync/volunteersync/repository/LocationRepository.java
@@ -3,6 +3,7 @@ package com.daniel4k.volunteersync.volunteersync.repository;
 import com.daniel4k.volunteersync.volunteersync.model.Location;
 import com.daniel4k.volunteersync.volunteersync.model.LocationType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
     Location findByCode(String code);
@@ -10,4 +11,10 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
     boolean existsByCode(String code);
 
     Location findByNameAndType(String name, LocationType type);
+    // 1. To fetch the top level (Provinces)
+    List<Location> findByType(LocationType type);
+
+    // 2. To fetch the children (Districts, Sectors, Cells, Villages)
+    List<Location> findByParent_LocationId(Long parentId);
+
 }
